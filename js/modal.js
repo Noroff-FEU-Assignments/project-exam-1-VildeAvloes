f00de9a8ca46088ac1528c380f783453cd296c40
VildeAvloes <94361976+VildeAvloes@users.noreply.github.com>
2023-04-06 15:18:03 +0200

    created modal for images

---
+++ js/modal.js
@@ -0,0 +1,33 @@
+const modal = document.getElementById("modalWindow");
+const close = document.getElementsByClassName("close")[0];
+var image = document.querySelector(".modal-image");
+var modalImage = document.querySelectorAll(".modal-image img");
+var modalImg = document.querySelector(".modal-content");
+var captionText = document.querySelector(".caption");
+var imgSrc;
+var caption;
+
+//fetch imagesrc
+modalImage.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    var imgSrc = e.target.src;
+    var caption = e.target.alt;
+    console.log(imgSrc, caption);
+
+    modal.style.display = "block";
+    modalImg.src = imgSrc;
+    captionText.innerHTML = caption;
+  });
+});
+
+//display image and caption in modal
+// image.onclick = function () {
+//   modal.style.display = "block";
+//   modalImg.src = imgSrc;
+//   captionText.innerHTML = caption;
+// };
+
+//close function
+close.onclick = () => {
+  modal.style.display = "none";
+};
