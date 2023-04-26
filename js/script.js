710e18ef5f8371289452a5d9ae0c8a6d9d7f4738
VildeAvloes <94361976+VildeAvloes@users.noreply.github.com>
2023-04-26 15:45:44 +0200

    fixed logo, i think ...

--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
 import { hamburgerMenu } from "./components/hamburger.js";
 import { createModal } from "./modal.js";
 import { renderSlider } from "./slider.js";
-import { validateForm } from "./contact.js";
-import { getPostDetails } from "./blog-post.js";
-import { renderPosts } from "./blog.js";
 
 hamburgerMenu();
 
 createModal();
 
 renderSlider();
-
-validateForm();
-
-renderPosts();
-
-getPostDetails();
